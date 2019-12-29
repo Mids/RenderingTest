96472fb6496ad0cbdc617ede8a00648fed52f066
JinPark <jiny199109@gmail.com>
2019-12-29 18:07:26 +0900

    Load mesh

---
+++ Assets/02.Scripts/FaceModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceModel : MonoBehaviour
+{
+
+	private Vector3[] _vertexList;
+
+	private int[] _faceList;
+
+	private Mesh _mesh;
+
+	private FileParser _fileParser;
+
+	private MeshFilter _meshFilter;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_fileParser = GetComponent<FileParser>();
+		_meshFilter = GetComponent<MeshFilter>();
+
+		StartCoroutine(LoadFace());
+	}
+
+	IEnumerator LoadFace()
+	{
+		_vertexList = _fileParser.GetVertices();
+		_faceList = _fileParser.GetFacet();
+		_mesh = new Mesh();
+
+		_meshFilter.mesh = _mesh;
+		_mesh.name = "Face";
+		_mesh.vertices = _vertexList;
+		_mesh.triangles = _faceList;
+		
+		_mesh.RecalculateNormals();
+		
+		print("Load Face Done");
+		yield break;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/02.Scripts/FileParser.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FileParser : MonoBehaviour
+{
+	public TextAsset VertexFile;
+	public TextAsset FaceFile;
+
+	private StreamReader _vertexReader;
+	public int vectexCount;
+
+	private void Start()
+	{
+		_vertexReader = new StreamReader(new MemoryStream(VertexFile.bytes));
+		vectexCount = Convert.ToInt32(_vertexReader.ReadLine());
+
+		print($"vertex count : {vectexCount}");
+	}
+
+
+	public Vector3[] GetVertices()
+	{
+		var vertices = new Vector3[vectexCount];
+
+		for (int i = 0; i < vectexCount; i++)
+		{
+			string line = _vertexReader.ReadLine();
+			string[] bits = line?.Split('\t');
+			if (bits == null)
+			{
+				print("bits is null in faceData.");
+				break;
+			}
+
+			if (bits.Length != 3)
+			{
+				print($"There are {bits.Length} bits in one line.");
+				break;
+			}
+
+			vertices[i] = new Vector3(Convert.ToSingle(bits[0]), Convert.ToSingle(bits[1]), Convert.ToSingle(bits[2]));
+		}
+
+
+		return vertices;
+	}
+
+	public int[] GetFacet()
+	{
+		StreamReader faceReader = new StreamReader(new MemoryStream(FaceFile.bytes));
+		int count = Convert.ToInt32(faceReader.ReadLine());
+
+		print($"face count : {count}");
+
+		var facet = new int[count * 3];
+
+		for (int i = 0; i < count; i++)
+		{
+			string line = faceReader.ReadLine();
+			string[] bits = line?.Split('\t');
+			if (bits == null)
+			{
+				print("bits is null in faceData.");
+				break;
+			}
+
+			if (bits.Length != 3)
+			{
+				print($"There are {bits.Length} bits in one line.");
+				break;
+			}
+
+			for (int j = 0; j < 3; j++)
+			{
+				facet[i * 3 + j] = Convert.ToInt32(bits[j]);
+			}
+		}
+
+		faceReader.Close();
+
+		return facet;
+	}
+
+	private void OnDestroy()
+	{
+		_vertexReader.Close();
+	}
+}
